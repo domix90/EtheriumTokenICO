1cf6a486560bd4a09712ffc736058a84c6f9c197
Dsosen <dominikandsosnowski@gmail.com>
2019-02-08 17:57:05 +0100

    Initial commit for the smart contract creation of the token.
    Created the contract and set the Solidity to the proper version of 0.4.2.

---
+++ BeitCoin.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract BeitCoin {
+	// Constructor function 
+	// Set the total number of tokens 
+	// Read the total number of tokens 
+	function BeitCoin () public {
+		
+		
+	}
+}
